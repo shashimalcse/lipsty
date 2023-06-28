a49747265c0abed78417fd6244ba659016819a5c
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2023-06-28 23:29:50 +0530

    define the grammer

--- parsing.c
+++ parsing.c
@@ -1,45 +1,44 @@
 #include "mpc.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-#ifdef _WIN32
-#include <string.h>
-
-static char buffer[2048]
-
-    char *
-    readline(char *prompt)
-{
-
-    fputs(prompt, stdout);
-    fgets(buffer, 2048, stdin);
-    char *cpy = malloc(strlen(buffer) + 1);
-    strcpy(cpy, buffer);
-    cpy[strlen(cpy) - 1] = '\0';
-    return cpy
-}
-
-void add_history(char *unused) {}
-
-#else
 #include <editline/readline.h>
-#endif
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     /* Parsers for math */
     mpc_parser_t* Number = mpc_new("number");
-    mpc_parser_t* Oparator = mpc_new("oparator");
+    mpc_parser_t* Operator = mpc_new("operator");
     mpc_parser_t* Expr = mpc_new("expr");
     mpc_parser_t* Lispty = mpc_new("lispty");
 
-    while (1)
-    {
+    /* Define language */
+    mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      lispty    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Lispty);
 
-        char *input = readline("lipsty> ");
+    while (1) {
+
+        char* input = readline("lispy> ");
         add_history(input);
-        printf("No you're a %s\n", input);
+
+        /* parse the user input */
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispty, &r))
+        {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
         free(input);
     }
+
+    /* undefine and delete  parsers */
+    mpc_cleanup(4, Number, Operator, Expr, Lispty);
     return 0;
 }
